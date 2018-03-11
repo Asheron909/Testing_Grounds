cd00eea2d155ea40b3ccfbaa40bd4aa56ac89c17
Josh Deal <asheron909@gmail.com>
2018-03-11 16:13:15 -0700

    Added AI mechanic to set focus, Added events to be suspicious and notice player behavior, Added unsuspicious and return patrol behavior, Added decorators to behavior so that the AI would not get hung up and lag processing, Edited Guard and Player Anim to make them properly hold guns with 2 Bone IK (Still can add more to make finger look better and not clip gun)

---
+++ Source/TestingGrounds/Weapons/Weapon.h
@@ -0,0 +1,61 @@
+// Copyright Josh "Asheron" Deal 2018
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+UCLASS()
+class TESTINGGROUNDS_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+
+	/** Gun mesh: 1st person view (seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	class USkeletalMeshComponent* FP_Gun;
+
+	/** Location on gun mesh where projectiles should spawn. */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	class USceneComponent* FP_MuzzleLocation;
+
+	/** Gun mesh: VR view (attached to the VR controller directly, no arm, just the actual gun) */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	class USkeletalMeshComponent* VR_Gun;
+
+	/** Location on VR gun mesh where projectiles should spawn. */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	class USceneComponent* VR_MuzzleLocation;
+
+public:	
+	// Sets default values for this actor's properties
+	AWeapon();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	/** Projectile class to spawn */
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+	TSubclassOf<class ABallProjectile> ProjectileClass;
+
+	/** Sound to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	class USoundBase* FireSound;
+
+	/** AnimMontage to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	class UAnimMontage* FireAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	class UAnimInstance* AnimInstance;
+
+	/** Whether to use motion controller location for aiming. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	uint32 bUsingMotionControllers : 1;
+
+	/** Fires a projectile. */
+	void OnFire();
+	
+};
